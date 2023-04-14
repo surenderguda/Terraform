c3422970c59322c8f3a20b90834e901076c3890b
surenderguda <56069143+surenderguda@users.noreply.github.com>
2023-04-14 13:08:36 +0530

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = "us-west-2"
 }
-resource "aws_iam_user" "lb" {
-  name = "loadbalancer"
-  path = "/system/"
+
+resource "aws_instance" "myec2" {
+   ami = "ami-082b5a644766e0e6f"
+   instance_type = "t2.micro"
 }
